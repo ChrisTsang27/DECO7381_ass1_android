fc8ca12270e930a1c11e1a87c2b5f825854b241e
Chris <zengzhi19920527@live.com>
2021-08-25 17:33:11 +0800

    Some more change on the layout and onclick listener on the app.

---
+++ app/src/main/java/com/assignment1/deco7381_assignment1_ver1/LevelPageActivity.java
@@ -0,0 +1,14 @@
+package com.assignment1.deco7381_assignment1_ver1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_page);
+    }
+}
--- app/src/main/java/com/assignment1/deco7381_assignment1_ver1/MainActivity.java
+++ app/src/main/java/com/assignment1/deco7381_assignment1_ver1/MainActivity.java
@@ -2,13 +2,27 @@ package com.assignment1.deco7381_assignment1_ver1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnSelectLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnSelectLevel = findViewById(R.id.btn_start);
+        mBtnSelectLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Move to other page by clicking the button.
+                Intent intent = new Intent(MainActivity.this, LevelPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
